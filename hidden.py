78b69aa96115a27fe819cf216b7c13ad4f67ef33
wilsonfong56 <63813902+wilsonfong56@users.noreply.github.com>
2021-12-24 17:33:38 -0800

    Update hidden.py

--- hidden.py
+++ hidden.py
@@ -1,6 +1,7 @@
+#Fill in definitions with your own twitter issued keys and token.
 def oauth():
-    return {"consumer_key": "jhsBRqjHPaV1RkNycL6JCMjlC",
-            "consumer_key_secret": "TppveXzOoaoUS3iHHL7aEFefcJFiGm3tSY65kknxGC7OPqesmE",
-            "bearer_token": "AAAAAAAAAAAAAAAAAAAAANZWXAEAAAAA%2F8CTbhg3PWx9rzb212bGSsZ2XfA%3Dr2nkDRVXkTPE72lndkYyVDaQzw8xn7flRWADXSDLYzWrgXQxNk",
-            "access_token": "709828902-ftfc77TS9myovVFnPbniq76bwjVTASTXAZJFFyst",
-            "access_token_secret": "0WXjMzlE6O8xFrudVswP4zyc66129SHwmA0ph7BxXCATa"}
+    return {"consumer_key": "...",
+            "consumer_key_secret": "...",
+            "bearer_token": "...",
+            "access_token": "...",
+            "access_token_secret": "..."}
