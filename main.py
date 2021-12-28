2f59f8133edaee9739de1abe8f22c2eec0b35072
wilsonfong56 <63813902+wilsonfong56@users.noreply.github.com>
2021-12-28 11:33:58 -0800

    Generalized helper functions

--- main.py
+++ main.py
@@ -3,32 +3,42 @@ import functions
 import schedule
 import time
 
-def main():
+def term_search_write(search_str, re_expression, file_name, conditional):
     print("Working...")
-    ticker_list = functions.timeline_search("$", "[$][a-zA-Z]+")
-    file1 = open("tickers.txt", 'a')
-    for ticker in ticker_list:
-        if len(ticker) < 7:
-            file1.write(ticker+"\n")
+    terms_list = functions.timeline_search(search_str, re_expression)
+    file1 = open(file_name, 'a')
+    for term in terms_list:
+        if conditional(term):
+            file1.write(term+"\n")
     file1.close()
 
-def run(f):
-    schedule.every(5).minutes.do(f)
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+def term_search_dict(terms_dict, search_str, re_expression, conditional):
+    print("Working...")
+    terms_list = functions.timeline_search(search_str, re_expression)
+    for term in terms_list:
+        if conditional(term):
+            if term not in terms_dict:
+                terms_dict[term] = 1
+            else:
+                terms_dict[term] += 1
+    
 
-def print_tickers_dict(file_name):
-    tickers_dict = {}
+def print_terms_from_file(file_name):
+    terms_dict = {}
     f = open(file_name, "r")
-    for ticker in f.readlines():
-        if ticker.strip() not in tickers_dict:
-            tickers_dict[ticker.strip()] = 1
+    for term in f.readlines():
+        if term.strip() not in terms_dict:
+            terms_dict[term.strip()] = 1
         else:
-            tickers_dict[ticker.strip()] += 1
-    for i in sorted(tickers_dict.items(), key=lambda kv:(kv[1], kv[0]), reverse=True):
+            terms_dict[term.strip()] += 1
+    for i in sorted(terms_dict.items(), key=lambda kv:(kv[1], kv[0]), reverse=True):
         print(i[0][1:] + " "*(6-len(i[0])) + "     " + str(i[1]))
 
 if __name__ == "__main__":
-    #run(main)
-    print_tickers_dict("tickers.txt")
+    term_dict = {}
+    schedule.every(5).minutes.do(term_search_dict, term_dict, "$", "[$][a-zA-Z]+", "tickers.txt", lambda x : len(x) < 7)
+    #schedule.every(5).minutes.do(term_search_write, "$", "[$][a-zA-Z]+", "tickers.txt", lambda x : len(x) < 7)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+    print_terms_from_file("tickers.txt")
